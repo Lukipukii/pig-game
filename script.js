05244c0f8114c1aada6c3fedb0e5223862b3d874
Lucas Colban <lukipuki.git@gmail.com>
2024-01-25 13:14:40 +0100

    Añadidas constantes árbol DOM y funcion para reiniciar a 0 la puntuacion

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const scorePlayer1 = document.getElementById('score--0')
+const scorePlayer2 = document.getElementById('score--1')
+const currentScore1 = document.getElementById('current--0')
+const currentScore2 = document.getElementById('current--1')
+const dice = document.querySelector('.dice')
 
+document.querySelector('.btn--new').addEventListener('click', function () {
+  scorePlayer1.textContent = '0'
+  scorePlayer2.textContent = '0'
+  currentScore1.textContent = '0'
+  currentScore2.textContent = '0'
+})
+
+/*
+document.querySelector('.btn--roll').addEventListener('click', function () {
+  const isPlayerActive = player.classList.contains('.player--active')
+  if(document.querySelector('.player--0').isPlayerActive) {
+
+  } else {
+
+  }
+  
+
+  let rndNumber = Math.floor(Math.random() * 6) + 1
+  let diceSrc = `./dice-${rndNumber}.png`
+  dice.src = diceSrc
+  currentScore += rndNumber
+  
+})
+
+document.querySelector('.btn--hold').addEventListener('click', function () {
+})
+*/
