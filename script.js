77324e5c8acbb9a81a879c22100f150d453f1df2
Lucas Colban <lukipuki.git@gmail.com>
2024-01-25 14:06:03 +0100

    Fix del evento roll, añadido condición roll=1 y funcion cambio jugador

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Elemento del arbol dom
 const player1 = document.querySelector('.player--0')
 const player2 = document.querySelector('.player--1')
-const playerActive = document.querySelector('.player--active')
 const scorePlayer1 = document.getElementById('score--0')
 const scorePlayer2 = document.getElementById('score--1')
 const currentScore1 = document.getElementById('current--0')
@@ -21,16 +20,39 @@ function addScore(currentScore) {
   let rndNum = Math.floor(Math.random() * 6) + 1
   let imgDice = `./dice-${rndNum}.png`
   dice.src = imgDice
-
-  currentScore.textContent = Number(currentScore.textContent) + rndNum
+  if (rndNum == 1) {
+    currentScore.textContent = 0
+    changeActivePlayer()
+  } else {
+    currentScore.textContent = Number(currentScore.textContent) + rndNum
+  }
 }
 
+//funcion para cambiar jugador activo
+function changeActivePlayer() {
+  player1.classList.toggle('player--active')
+  player2.classList.toggle('player--active')
+}
+
+//evento de ir sumando las tiradas
 document.querySelector('.btn--roll').addEventListener('click', function () {
-  if (player1.classList.contains(playerActive)) {
+  if (player1.classList.contains('player--active')) {
     addScore(currentScore1)
   } else {
     addScore(currentScore2)
   }
 })
 
-document.querySelector('.btn--hold').addEventListener('click', function () {})
+/*
+document.querySelector('.btn--hold').addEventListener('click', function () {
+  if (player1.classList.contains(playerActive)) {
+    scorePlayer1.textContent =
+      Number(scorePlayer1.textContent) + Number(currentScore1.textContent)
+    player1.classList.remove(playerActive)
+  } else {
+    scorePlayer2.textContent =
+      scorePlayer2.textContent + currentScore2.textContent
+    player2.classList.remove(playerActive)
+  }
+})
+*/
