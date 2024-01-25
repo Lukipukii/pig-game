157600e62532bd01898079f54032f8c3d0988872
Lucas Colban <lukipuki.git@gmail.com>
2024-01-25 13:37:40 +0100

    Añadida función de sumar tiradas

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//Elemento del arbol dom
+const player1 = document.querySelector('.player--0')
+const player2 = document.querySelector('.player--1')
+const playerActive = document.querySelector('.player--active')
 const scorePlayer1 = document.getElementById('score--0')
 const scorePlayer2 = document.getElementById('score--1')
 const currentScore1 = document.getElementById('current--0')
 const currentScore2 = document.getElementById('current--1')
 const dice = document.querySelector('.dice')
 
+//funcion para reiniciar valores a 0
 document.querySelector('.btn--new').addEventListener('click', function () {
   scorePlayer1.textContent = '0'
   scorePlayer2.textContent = '0'
@@ -11,23 +16,21 @@ document.querySelector('.btn--new').addEventListener('click', function () {
   currentScore2.textContent = '0'
 })
 
-/*
+//funcion para ir sumando la puntuacion
+function addScore(currentScore) {
+  let rndNum = Math.floor(Math.random() * 6) + 1
+  let imgDice = `./dice-${rndNum}.png`
+  dice.src = imgDice
+
+  currentScore.textContent = Number(currentScore.textContent) + rndNum
+}
+
 document.querySelector('.btn--roll').addEventListener('click', function () {
-  const isPlayerActive = player.classList.contains('.player--active')
-  if(document.querySelector('.player--0').isPlayerActive) {
-
+  if (player1.classList.contains(playerActive)) {
+    addScore(currentScore1)
   } else {
-
+    addScore(currentScore2)
   }
-  
-
-  let rndNumber = Math.floor(Math.random() * 6) + 1
-  let diceSrc = `./dice-${rndNumber}.png`
-  dice.src = diceSrc
-  currentScore += rndNumber
-  
 })
 
-document.querySelector('.btn--hold').addEventListener('click', function () {
-})
-*/
+document.querySelector('.btn--hold').addEventListener('click', function () {})
